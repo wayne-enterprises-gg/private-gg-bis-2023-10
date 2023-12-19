fc0bb6d82787a127fe883c88606cd7286cfc1f82
Lucius Fox <lucius-fox-gg@protonmail.com>
2023-12-19 18:03:17 +0100

    Updating app.py

--- app.py
+++ app.py
@@ -1,18 +1,6 @@
 import antigravity
 
-CERT="""-----BEGIN DSA PRIVATE KEY-----
-MIIBvAIBAAKBgQCnk/342VHlsC/b45pnyG0VvoADAPopaiB2L/Ccb/68QMtvkGDL
-3Xo5TfMy11LWaqZ7wJ8lJm2DBFlfn8HQCpp0mMeRd36l9IkPPT0+mHnq81b4Clij
-gOqqCwSfxfTjyOgv10WeqEC+asMnzRPC+Wg2vBrQLKXp5HUpls8bwul9IwIVAJKi
-oR6WEkuhULPDznc6Rdz4FEPrAoGAD0rRDf+rVG1QY6aWYNn33snVY0kNOZiZP4zp
-YksoOP7kQIwMzaJiBCaRolafEy2lwExrbmjXUve7Wuv6SRknm6s8dIktOQpGP9CK
-8rxt1CcHQha/++noTTIl5OlS9j+WStvY04eCmqTcIf+9Tf9Qu4UZSj/DTuX5SWbC
-iGPaQSECgYEAkso2U412ALuOhW4fC2Aa4x7ZcgY+HbMYBtsS+fCM1mJa/StQ+mxJ
-bzVUQdhjIAzWAjsag/TwYnQv7QefNtH9AMyiBAb05/qJid3yYw05CQzQtS9eJ8l1
-UQovVDrKVjzYnXbu/R4h6M0BCvZZGAv0Y/g3IvnSdR66gdZ/gSjbfhcCFQCK7GuP
-33IyGyXv6p+rCgiIlnSGNQ==
------END DSA PRIVATE KEY-----
-"""
+SEED=824684b8-9fb1-4183-b867-de87f55d3bd5
 
 def main():
     antigravity.fly()
